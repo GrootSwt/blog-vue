531e6368bb906f42af2a03c4ef422985508e9ba0
GrootSwt <396518038@qq.com>
2022-08-23 09:07:08 +0800

    替换偏爱图标

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4444',
+        target: 'http://localhost:8889',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
